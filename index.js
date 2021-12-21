d5158242b6df558fd51c2f4fb9d386b40c4876a8
Kelerik <vincent.ng39@gmail.com>
2021-12-21 18:35:46 -0500

    engineer and intern prompts

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const inquirer = require("inquirer");
 
-// prompt user
-const promptUser = () => {
-   console.log("Welcome!");
+// prompt manager
+const promptManager = () => {
    return inquirer.prompt([
       {
          type: "input",
@@ -17,15 +16,75 @@ const promptUser = () => {
          validate: (promptInput) => validateInput(promptInput),
       },
       {
-         type: "string",
+         type: "input",
          name: "managerEmail",
          message: "Please enter the team manager's email address",
          validate: (promptInput) => validateInput(promptInput),
       },
       {
-         type: "string",
+         type: "input",
          name: "managerOfficeNumber",
          message: "Please enter the team manager's office phone number",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+   ]);
+};
+
+// prompt engineer
+const promptEngineer = () => {
+   return inquirer.prompt([
+      {
+         type: "input",
+         name: "engineerName",
+         message: "Please enter the engineer's name",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+      {
+         type: "input",
+         name: "engineerId",
+         message: "Please enter the engineer's employee ID",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+      {
+         type: "input",
+         name: "engineerEmail",
+         message: "Please enter the engineer's email address",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+      {
+         type: "input",
+         name: "engineerGithub",
+         message: "Please enter the engineer's Github username",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+   ]);
+};
+
+// prompt intern
+const promptIntern = () => {
+   return inquirer.prompt([
+      {
+         type: "input",
+         name: "internName",
+         message: "Please enter the intern's name",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+      {
+         type: "input",
+         name: "internId",
+         message: "Please enter the intern's employee ID",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+      {
+         type: "input",
+         name: "internEmail",
+         message: "Please enter the intern's email address",
+         validate: (promptInput) => validateInput(promptInput),
+      },
+      {
+         type: "input",
+         name: "internSchool",
+         message: "Please enter the name of the intern's school",
          validate: (promptInput) => validateInput(promptInput),
       },
    ]);
@@ -42,6 +101,10 @@ const validateInput = (string) => {
 };
 
 // execute
-promptUser().then((promptOutput) => {
-   console.log(promptOutput);
-});
+console.log("Welcome!");
+promptManager()
+   .then((managerOutput) => console.log(managerOutput))
+   .then(() => promptEngineer())
+   .then((engineerOutput) => console.log(engineerOutput))
+   .then(() => promptIntern())
+   .then((internOutput) => console.log(internOutput));
